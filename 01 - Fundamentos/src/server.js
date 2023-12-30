16bb1413e51101ac79652489e23f3398dd4110b7
Maciel Júnior <maciel_custodio@icloud.com>
2023-12-29 22:08:59 -0300

    feat: :sparkles: save a user in memory

--- 01 - Fundamentos/src/server.js
+++ 01 - Fundamentos/src/server.js
@@ -1,7 +1,25 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((request, response) => {
-    return response.end('Hello World')
+    const { method, url } = request
+
+    if (method === 'GET' && url === '/users') {
+        return response
+        .setHeader('Content-Type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@email.com'
+        })
+
+        return response.end('Usuário criado com sucesso!')
+    }
 })
 
 server.listen(3333)

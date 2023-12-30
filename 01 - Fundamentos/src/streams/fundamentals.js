c0574fa28de6599a1680138127a33213f239543e
Maciel Júnior <maciel_custodio@icloud.com>
2023-12-30 19:50:59 -0300

    feat: :sparkles: create writable e transform streams

--- 01 - Fundamentos/src/streams/fundamentals.js
+++ 01 - Fundamentos/src/streams/fundamentals.js
@@ -2,7 +2,7 @@
 
 // process.stdin.pipe(process.stdout);
 
-import { Readable } from "node:stream";
+import { Readable, Writable, Transform } from "node:stream";
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -21,5 +21,20 @@ class OneToHundredStream extends Readable {
     }, 1000);
   }
 }
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
 
-new OneToHundredStream().pipe(process.stdout);
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+class MulyiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MulyiplyByTenStream());

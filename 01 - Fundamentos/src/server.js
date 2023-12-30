16724fa9662776fcccf75ed6f60881aae154f3ef
Maciel Júnior <maciel_custodio@icloud.com>
2023-12-29 22:13:49 -0300

    feat: :sparkles: add status codes 201 and 404

--- 01 - Fundamentos/src/server.js
+++ 01 - Fundamentos/src/server.js
@@ -1,26 +1,29 @@
-import http from 'node:http'
+import http from "node:http";
 
-const users = []
+const users = [];
 
 const server = http.createServer((request, response) => {
-    const { method, url } = request
+  const { method, url } = request;
 
-    if (method === 'GET' && url === '/users') {
-        return response
-        .setHeader('Content-Type', 'application/json')
-        .end(JSON.stringify(users))
-    }
+  if (method === "GET" && url === "/users") {
+    return response
+      .setHeader("Content-Type", "application/json")
+      .end(JSON.stringify(users));
+  }
 
-    if (method === 'POST' && url === '/users') {
-        users.push({
-            id: 1,
-            name: 'John Doe',
-            email: 'johndoe@email.com'
-        })
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@email.com",
+    });
 
-        return response.end('Usuário criado com sucesso!')
-    }
-})
+    return response.writeHead(201).end();
+    // O código 201 é retornado quando um novo recurso é criado
+  }
 
-server.listen(3333)
+  return response.writeHead(404).end();
+  // O código 404 é retornado quando a rota não é encontrada
+});
 
+server.listen(3333);

c7117000e711bd2e91e89c2de14b730c6798da01
Maciel Júnior <maciel_custodio@icloud.com>
2024-01-01 21:56:13 -0300

    feat: :sparkles: build route path regex

--- 01 - Fundamentos/src/server.js
+++ 01 - Fundamentos/src/server.js
@@ -1,10 +1,19 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
 
   await json(request, response);
+
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url;
+  });
+
+  if (route) {
+    return route.handler(request, response);
+  }
 
   return response.writeHead(404).end();
 });

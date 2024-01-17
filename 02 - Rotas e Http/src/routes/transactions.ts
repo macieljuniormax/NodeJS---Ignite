4e30e35ee0f87b15b17ad77edc1e2d8d87413649
Maciel Júnior <maciel_custodio@icloud.com>
2024-01-16 21:20:56 -0300

    feat: :sparkles: create method get to show all transactions

--- 02 - Rotas e Http/src/routes/transactions.ts
+++ 02 - Rotas e Http/src/routes/transactions.ts
@@ -4,6 +4,12 @@ import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
   app.post('/', async (request, response) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),

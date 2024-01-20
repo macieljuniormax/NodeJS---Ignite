b73c78ff97ddd86fcb4370c8b76172d0ca23b9ff
Maciel Júnior <maciel_custodio@icloud.com>
2024-01-20 12:24:18 -0300

    test: :test_tube: it should be able to list all transactions

--- 02 - Rotas e Http/test/transactions.spec.ts
+++ 02 - Rotas e Http/test/transactions.spec.ts
@@ -1,6 +1,7 @@
 import { expect, it, beforeAll, afterAll, describe } from 'vitest'
 import { app } from '../src/app'
 import request from 'supertest'
+import { title } from 'node:process'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -12,12 +13,37 @@ describe('Transactions routes', () => {
   })
 
   it('should be able to create a new transaction', async () => {
-    const response = await request(app.server).post('/transactions').send({
-      title: 'New Transaction',
-      amount: 5000,
-      type: 'credit',
-    })
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 
-    expect(response.statusCode).toEqual(201)
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })

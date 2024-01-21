9c37b47bf183dd9b4323cc16b668305315a7f4c2
Maciel Júnior <maciel_custodio@icloud.com>
2024-01-21 18:52:43 -0300

    test: :test_tube: should be able to get a specific transaction

--- 02 - Rotas e Http/test/transactions.spec.ts
+++ 02 - Rotas e Http/test/transactions.spec.ts
@@ -1,5 +1,5 @@
 import { expect, it, beforeAll, afterAll, describe, beforeEach } from 'vitest'
-import { exec, execSync } from 'node:child_process'
+import { execSync } from 'node:child_process'
 import { app } from '../src/app'
 import request from 'supertest'
 
@@ -50,5 +50,36 @@ describe('Transactions routes', () => {
         amount: 5000,
       }),
     ])
+  })
+
+  it('should be able to get a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    )
   })
 })

13af9255bca268027b46a2a791fabefd97f3d78f
Maciel Júnior <maciel_custodio@icloud.com>
2024-01-24 20:52:17 -0300

    feat: :sparkles: configure version to postgres

--- 02 - Rotas e Http/src/database.ts
+++ 02 - Rotas e Http/src/database.ts
@@ -3,6 +3,7 @@ import { env } from './env'
 
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
+  version: env.DATABASE_CLIENT === 'sqlite' ? 'latest' : '15',
   connection:
     env.DATABASE_CLIENT === 'sqlite'
       ? {

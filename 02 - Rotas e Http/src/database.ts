e791cc5b6b9c28fd7c98a26643bda0db203ef6d1
Maciel Júnior <maciel_custodio@icloud.com>
2024-01-24 21:05:40 -0300

    fix: :bug: fixed knex.fn.now( )

--- 02 - Rotas e Http/src/database.ts
+++ 02 - Rotas e Http/src/database.ts
@@ -3,7 +3,6 @@ import { env } from './env'
 
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  version: env.DATABASE_CLIENT === 'sqlite' ? 'latest' : '15',
   connection:
     env.DATABASE_CLIENT === 'sqlite'
       ? {
